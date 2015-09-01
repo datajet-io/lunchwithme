00e25795685fd6fe7439d169ef7cd4e8fc07b1bb
petard <petar.djekic@rocket-internet.de>
2015-09-01 16:47:27 +0200

    new ux

---
+++ LunchWithMe/UserNameListTableViewController.swift
@@ -0,0 +1,145 @@
+//
+//  UserNameListTableViewController.swift
+//  LunchWithMe
+//
+//  Created by zeus on 01/09/15.
+//  Copyright (c) 2015 disco. All rights reserved.
+//
+
+import UIKit
+import MultipeerConnectivity
+
+class UserNameListTableViewController: UITableViewController, MCBrowserViewControllerDelegate, MCSessionDelegate {
+    
+    let serviceType = "LunchWithMe"
+    
+    var userName: String = ""
+    
+    var browser : MCBrowserViewController!
+    var assistant : MCAdvertiserAssistant!
+    var session : MCSession!
+    var peerID: MCPeerID!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.peerID = MCPeerID(displayName: userName)
+        
+        self.session = MCSession(peer: peerID)
+        self.session.delegate = self
+        
+        // create the browser viewcontroller with a unique service name
+        self.browser = MCBrowserViewController(serviceType:serviceType,
+            session:self.session)
+        
+        self.browser.maximumNumberOfPeers = 4
+        
+        self.browser.delegate = self;
+        
+        self.assistant = MCAdvertiserAssistant(serviceType:serviceType,
+            discoveryInfo:nil, session:self.session)
+        
+        // tell the assistant to start advertising our fabulous chat
+        self.assistant.start()
+                
+        self.navigationItem.setRightBarButtonItem(UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "showBrowser:"), animated: true)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return self.session.connectedPeers.count
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("userNameCellIdentifier", forIndexPath: indexPath) as! UITableViewCell
+        
+        let buddyName = session.connectedPeers[indexPath.row].displayName
+        
+        cell.textLabel?.text = buddyName
+
+        return cell
+    }
+
+    // MARK: - Peer
+    @IBAction func showBrowser(sender: UIButton) {
+        // Show the browser view controller
+        self.presentViewController(self.browser, animated: true, completion: nil)
+    }
+    
+    func browserViewControllerDidFinish(
+        browserViewController: MCBrowserViewController!)  {
+            // Called when the browser view controller is dismissed (ie the Done
+            // button was tapped)
+            
+            self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func browserViewControllerWasCancelled(
+        browserViewController: MCBrowserViewController!)  {
+            // Called when the browser view controller is cancelled
+            
+            self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func session(session: MCSession!, didReceiveData data: NSData!,
+        fromPeer peerID: MCPeerID!)  {
+            // Called when a peer sends an NSData to us
+            
+            // This needs to run on the main queue
+            dispatch_async(dispatch_get_main_queue()) {
+                
+                var msg = NSString(data: data, encoding: NSUTF8StringEncoding)
+                
+                //                self.updateChat(msg! as String, fromPeer: peerID)
+            }
+    }
+    
+    // The following methods do nothing, but the MCSessionDelegate protocol
+    // requires that we implement them.
+    func session(session: MCSession!,
+        didStartReceivingResourceWithName resourceName: String!,
+        fromPeer peerID: MCPeerID!, withProgress progress: NSProgress!)  {
+            
+            // Called when a peer starts sending a file to us
+    }
+    
+    func session(session: MCSession!,
+        didFinishReceivingResourceWithName resourceName: String!,
+        fromPeer peerID: MCPeerID!,
+        atURL localURL: NSURL!, withError error: NSError!)  {
+            // Called when a file has finished transferring from another peer
+    }
+    
+    func session(session: MCSession!, didReceiveStream stream: NSInputStream!,
+        withName streamName: String!, fromPeer peerID: MCPeerID!)  {
+            // Called when a peer establishes a stream with us
+    }
+    
+    func session(session: MCSession!, peer peerID: MCPeerID!,
+        didChangeState state: MCSessionState)  {
+            
+            if state == MCSessionState.Connected {
+                dispatch_async(dispatch_get_main_queue()) {
+                    
+                    self.tableView.reloadData()
+                }
+            }
+    }
+
+
+}
--- LunchWithMe/ViewController.swift
+++ LunchWithMe/ViewController.swift
@@ -7,145 +7,26 @@
 //
 
 import UIKit
-import MultipeerConnectivity
 
-class ViewController: UIViewController, MCBrowserViewControllerDelegate, MCSessionDelegate {
+class ViewController: UIViewController {
     
     let serviceType = "LunchWithMe"
     
-    var browser : MCBrowserViewController!
-    var assistant : MCAdvertiserAssistant!
-    var session : MCSession!
-    var peerID: MCPeerID!
-    
-   /* @IBOutlet var chatView: UITextView!
-    @IBOutlet var messageField: UITextField!*/
-    
-    
-    @IBOutlet weak var userListView: UITextView!
     @IBOutlet weak var userNameText: UITextField!
-    @IBOutlet weak var browseButton: UIButton!
+    @IBOutlet weak var startLookingForUsersButton: UIButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.browseButton.enabled = false
-        
+                
     }
     
-    @IBAction func submitUserName(sender: UIButton) {
-        self.peerID = MCPeerID(displayName: userNameText.text)
-    
-        sender.enabled = false
-        self.userNameText.enabled = false
-        
-        self.browseButton.enabled = true
-        
-        self.session = MCSession(peer: peerID)
-        self.session.delegate = self
-        
-        // create the browser viewcontroller with a unique service name
-        self.browser = MCBrowserViewController(serviceType:serviceType,
-            session:self.session)
-        
-        self.browser.maximumNumberOfPeers = 4
-        
-        self.browser.delegate = self;
-        
-        self.assistant = MCAdvertiserAssistant(serviceType:serviceType,
-            discoveryInfo:nil, session:self.session)
-        
-        // tell the assistant to start advertising our fabulous chat
-        self.assistant.start()
-    }
-    
-    
-/*    @IBAction func sendChat(sender: UIButton) {
-        // Bundle up the text in the message field, and send it off to all
-        // connected peers
-        
-        let msg = self.messageField.text.dataUsingEncoding(NSUTF8StringEncoding,
-            allowLossyConversion: false)
-        
-        var error : NSError?
-        
-        self.session.sendData(msg, toPeers: self.session.connectedPeers,
-            withMode: MCSessionSendDataMode.Unreliable, error: &error)
-        
-        if error != nil {
-            print("Error sending data: \(error?.localizedDescription)")
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let dest = segue.destinationViewController as? UserNameListTableViewController {
+            dest.userName = self.userNameText.text
         }
-        
-        self.updateChat(self.messageField.text, fromPeer: self.peerID)
-        
-        self.messageField.text = ""
     }
-*/
-    
-    @IBAction func showBrowser(sender: UIButton) {
-        // Show the browser view controller
-        self.presentViewController(self.browser, animated: true, completion: nil)
-    }
-    
-    func browserViewControllerDidFinish(
-        browserViewController: MCBrowserViewController!)  {
-            // Called when the browser view controller is dismissed (ie the Done
-            // button was tapped)
-            
-            self.dismissViewControllerAnimated(true, completion: nil)
-    }
-    
-    func browserViewControllerWasCancelled(
-        browserViewController: MCBrowserViewController!)  {
-            // Called when the browser view controller is cancelled
-            
-            self.dismissViewControllerAnimated(true, completion: nil)
-    }
-    
-    func session(session: MCSession!, didReceiveData data: NSData!,
-        fromPeer peerID: MCPeerID!)  {
-            // Called when a peer sends an NSData to us
-            
-            // This needs to run on the main queue
-            dispatch_async(dispatch_get_main_queue()) {
-                
-                var msg = NSString(data: data, encoding: NSUTF8StringEncoding)
-                
-//                self.updateChat(msg! as String, fromPeer: peerID)
-            }
-    }
-    
-    // The following methods do nothing, but the MCSessionDelegate protocol
-    // requires that we implement them.
-    func session(session: MCSession!,
-        didStartReceivingResourceWithName resourceName: String!,
-        fromPeer peerID: MCPeerID!, withProgress progress: NSProgress!)  {
-            
-            // Called when a peer starts sending a file to us
-    }
-    
-    func session(session: MCSession!,
-        didFinishReceivingResourceWithName resourceName: String!,
-        fromPeer peerID: MCPeerID!,
-        atURL localURL: NSURL!, withError error: NSError!)  {
-            // Called when a file has finished transferring from another peer
-    }
-    
-    func session(session: MCSession!, didReceiveStream stream: NSInputStream!,
-        withName streamName: String!, fromPeer peerID: MCPeerID!)  {
-            // Called when a peer establishes a stream with us
-    }
-    
-    func session(session: MCSession!, peer peerID: MCPeerID!,
-        didChangeState state: MCSessionState)  {
-            
-            if state == MCSessionState.Connected {
-                self.userListView.text = self.userListView.text + peerID.displayName
-            }
-            // Called when a connected peer changes state (for example, goes offline)
-            
-    }
-    
+
 }
 
